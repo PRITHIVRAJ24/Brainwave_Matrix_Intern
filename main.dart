25e62b6928d051144e940319fe083a26ef90d617
PRITHIVRAJ <116181493+PRITHIVRAJ24@users.noreply.github.com>
2024-07-19 21:35:41 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const TodoApp());
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Task-1',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const TodoListScreen(),
+    );
+  }
+}
+
+class TodoListScreen extends StatefulWidget {
+  const TodoListScreen({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _TodoListScreenState createState() => _TodoListScreenState();
+}
+
+class _TodoListScreenState extends State<TodoListScreen> {
+  final List<TodoItem> _todoItems = [];
+
+  final TextEditingController _textFieldController = TextEditingController();
+
+  void _addTodoItem(String task) {
+    if (task.isNotEmpty) {
+      setState(() {
+        _todoItems.add(TodoItem(task));
+      });
+      _textFieldController.clear();
+    }
+  }
+
+  void _removeTodoItem(int index) {
+    setState(() {
+      _todoItems.removeAt(index);
+    });
+  }
+
+  void _toggleTodoItem(int index) {
+    setState(() {
+      _todoItems[index].toggleDone();
+    });
+  }
+
+  Widget _buildTodoItem(TodoItem todoItem, int index) {
+    return ListTile(
+      title: Text(
+        todoItem.task,
+        style: TextStyle(
+          decoration: todoItem.isDone ? TextDecoration.lineThrough : null,
+        ),
+      ),
+      leading: Checkbox(
+        value: todoItem.isDone,
+        onChanged: (_) => _toggleTodoItem(index),
+      ),
+      trailing: IconButton(
+        icon: const Icon(Icons.delete),
+        onPressed: () => _removeTodoItem(index),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("To-Do"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: _todoItems.length,
+              itemBuilder: (context, index) {
+                return _buildTodoItem(_todoItems[index], index);
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _textFieldController,
+              decoration: InputDecoration(
+                hintText: 'Enter a task',
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.add),
+                  onPressed: () => _addTodoItem(_textFieldController.text),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TodoItem {
+  final String task;
+  bool isDone;
+
+  TodoItem(this.task, {this.isDone = false});
+
+  void toggleDone() {
+    isDone = !isDone;
+  }
+}
